4a78ede0083579c8b20afd95195e1f1218248c8b
First name Last name <58491627+Sukran1981@users.noreply.github.com>
2020-03-10 14:27:04 -0400

    homework

---
+++ src/test/java/com/cbt/tests/NavigationTests.java
@@ -0,0 +1,16 @@
+package com.cbt.tests;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class NavigationTests {
+    public static void main(String[] args) {
+
+
+        WebDriverManager.chromedriver().setup();
+
+        ChromeDriver driver=new ChromeDriver();
+
+        driver.get("http://google.com");
+    }
+}
---
+++ src/test/java/com/cbt/utilities/StringUtility.java
@@ -0,0 +1,10 @@
+package com.cbt.utilities;
+
+public class StringUtility {
+
+    public static void verifyEquals(String actual,String expected){
+
+
+
+    }
+}
