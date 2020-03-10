1232511691221993646474822e88595115d08ad8
First name Last name <58491627+Sukran1981@users.noreply.github.com>
2020-03-10 15:21:26 -0400

    homework

--- src/test/java/com/cbt/utilities/BrowserFactory.java
+++ src/test/java/com/cbt/utilities/BrowserFactory.java
@@ -1,9 +1,24 @@
 package com.cbt.utilities;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 public class BrowserFactory {
+    public static WebDriver createDriver(String browserName){
 
-if(browser.equalsIgnoreCase("chrome")){
-
+        if(browserName.equalsIgnoreCase("chrome")) {
+            WebDriverManager.chromedriver().version("79.0").setup();
+            return new ChromeDriver();
+        }else{
+            WebDriverManager.firefoxdriver().setup();
+            return new FirefoxDriver();
+        }
     }
-
 }
+
+
+
+
+
